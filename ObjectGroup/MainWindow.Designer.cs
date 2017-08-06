2b3a6d25c77facb63ea2531fc1b0898ef72ff3dc
Vadim Kuras <vakuras@microsoft.com>
2017-08-06 14:56:23 +0300

    Add code

---
+++ ObjectGroup/MainWindow.Designer.cs
@@ -0,0 +1,238 @@
+﻿namespace ObjectGroup
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this._picView = new System.Windows.Forms.PictureBox();
+            this._btnRandom = new System.Windows.Forms.Button();
+            this._btnSolve = new System.Windows.Forms.Button();
+            this._btnVisualSolve = new System.Windows.Forms.Button();
+            this._tmrSolve = new System.Windows.Forms.Timer(this.components);
+            this._lblSolving = new System.Windows.Forms.Label();
+            this._nudSpeed = new System.Windows.Forms.NumericUpDown();
+            this._btnStop = new System.Windows.Forms.Button();
+            this._nudGroups = new System.Windows.Forms.NumericUpDown();
+            this._lblGroups = new System.Windows.Forms.Label();
+            this._nudSize = new System.Windows.Forms.NumericUpDown();
+            this._lblSize = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this._picView)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this._nudSpeed)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this._nudGroups)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this._nudSize)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // _picView
+            // 
+            this._picView.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this._picView.Location = new System.Drawing.Point(12, 12);
+            this._picView.Name = "_picView";
+            this._picView.Size = new System.Drawing.Size(482, 482);
+            this._picView.TabIndex = 0;
+            this._picView.TabStop = false;
+            // 
+            // _btnRandom
+            // 
+            this._btnRandom.Location = new System.Drawing.Point(500, 95);
+            this._btnRandom.Name = "_btnRandom";
+            this._btnRandom.Size = new System.Drawing.Size(126, 32);
+            this._btnRandom.TabIndex = 1;
+            this._btnRandom.Text = "Randomize";
+            this._btnRandom.UseVisualStyleBackColor = true;
+            this._btnRandom.Click += new System.EventHandler(this._btnRandom_Click);
+            // 
+            // _btnSolve
+            // 
+            this._btnSolve.Location = new System.Drawing.Point(500, 133);
+            this._btnSolve.Name = "_btnSolve";
+            this._btnSolve.Size = new System.Drawing.Size(126, 32);
+            this._btnSolve.TabIndex = 2;
+            this._btnSolve.Text = "Fast Solve";
+            this._btnSolve.UseVisualStyleBackColor = true;
+            this._btnSolve.Click += new System.EventHandler(this._btnSolve_Click);
+            // 
+            // _btnVisualSolve
+            // 
+            this._btnVisualSolve.Location = new System.Drawing.Point(500, 171);
+            this._btnVisualSolve.Name = "_btnVisualSolve";
+            this._btnVisualSolve.Size = new System.Drawing.Size(126, 32);
+            this._btnVisualSolve.TabIndex = 3;
+            this._btnVisualSolve.Text = "Visual Solve";
+            this._btnVisualSolve.UseVisualStyleBackColor = true;
+            this._btnVisualSolve.Click += new System.EventHandler(this._btnVisualSolve_Click);
+            // 
+            // _tmrSolve
+            // 
+            this._tmrSolve.Tick += new System.EventHandler(this._tmrSolve_Tick);
+            // 
+            // _lblSolving
+            // 
+            this._lblSolving.AutoSize = true;
+            this._lblSolving.Location = new System.Drawing.Point(503, 14);
+            this._lblSolving.Name = "_lblSolving";
+            this._lblSolving.Size = new System.Drawing.Size(79, 13);
+            this._lblSolving.TabIndex = 4;
+            this._lblSolving.Text = "Solving Speed:";
+            // 
+            // _nudSpeed
+            // 
+            this._nudSpeed.Location = new System.Drawing.Point(588, 12);
+            this._nudSpeed.Maximum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this._nudSpeed.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this._nudSpeed.Name = "_nudSpeed";
+            this._nudSpeed.Size = new System.Drawing.Size(38, 20);
+            this._nudSpeed.TabIndex = 5;
+            this._nudSpeed.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this._nudSpeed.ValueChanged += new System.EventHandler(this._nudSpeed_ValueChanged);
+            // 
+            // _btnStop
+            // 
+            this._btnStop.Enabled = false;
+            this._btnStop.Location = new System.Drawing.Point(500, 209);
+            this._btnStop.Name = "_btnStop";
+            this._btnStop.Size = new System.Drawing.Size(126, 32);
+            this._btnStop.TabIndex = 6;
+            this._btnStop.Text = "Stop";
+            this._btnStop.UseVisualStyleBackColor = true;
+            this._btnStop.Click += new System.EventHandler(this._btnStop_Click);
+            // 
+            // _nudGroups
+            // 
+            this._nudGroups.Location = new System.Drawing.Point(588, 38);
+            this._nudGroups.Minimum = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            this._nudGroups.Name = "_nudGroups";
+            this._nudGroups.Size = new System.Drawing.Size(38, 20);
+            this._nudGroups.TabIndex = 8;
+            this._nudGroups.Value = new decimal(new int[] {
+            3,
+            0,
+            0,
+            0});
+            // 
+            // _lblGroups
+            // 
+            this._lblGroups.AutoSize = true;
+            this._lblGroups.Location = new System.Drawing.Point(538, 40);
+            this._lblGroups.Name = "_lblGroups";
+            this._lblGroups.Size = new System.Drawing.Size(44, 13);
+            this._lblGroups.TabIndex = 7;
+            this._lblGroups.Text = "Groups:";
+            // 
+            // _nudSize
+            // 
+            this._nudSize.Location = new System.Drawing.Point(588, 64);
+            this._nudSize.Maximum = new decimal(new int[] {
+            256,
+            0,
+            0,
+            0});
+            this._nudSize.Minimum = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            this._nudSize.Name = "_nudSize";
+            this._nudSize.Size = new System.Drawing.Size(38, 20);
+            this._nudSize.TabIndex = 10;
+            this._nudSize.Value = new decimal(new int[] {
+            128,
+            0,
+            0,
+            0});
+            // 
+            // _lblSize
+            // 
+            this._lblSize.AutoSize = true;
+            this._lblSize.Location = new System.Drawing.Point(552, 66);
+            this._lblSize.Name = "_lblSize";
+            this._lblSize.Size = new System.Drawing.Size(30, 13);
+            this._lblSize.TabIndex = 9;
+            this._lblSize.Text = "Size:";
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(636, 504);
+            this.Controls.Add(this._nudSize);
+            this.Controls.Add(this._lblSize);
+            this.Controls.Add(this._nudGroups);
+            this.Controls.Add(this._lblGroups);
+            this.Controls.Add(this._btnStop);
+            this.Controls.Add(this._nudSpeed);
+            this.Controls.Add(this._lblSolving);
+            this.Controls.Add(this._btnVisualSolve);
+            this.Controls.Add(this._btnSolve);
+            this.Controls.Add(this._btnRandom);
+            this.Controls.Add(this._picView);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "MainWindow";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "ObjectGroup";
+            ((System.ComponentModel.ISupportInitialize)(this._picView)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this._nudSpeed)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this._nudGroups)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this._nudSize)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox _picView;
+        private System.Windows.Forms.Button _btnRandom;
+        private System.Windows.Forms.Button _btnSolve;
+        private System.Windows.Forms.Button _btnVisualSolve;
+        private System.Windows.Forms.Timer _tmrSolve;
+        private System.Windows.Forms.Label _lblSolving;
+        private System.Windows.Forms.NumericUpDown _nudSpeed;
+        private System.Windows.Forms.Button _btnStop;
+        private System.Windows.Forms.NumericUpDown _nudGroups;
+        private System.Windows.Forms.Label _lblGroups;
+        private System.Windows.Forms.NumericUpDown _nudSize;
+        private System.Windows.Forms.Label _lblSize;
+    }
+}
+
---
+++ ObjectGroup/MainWindow.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace ObjectGroup
+{
+    public partial class MainWindow : Form
+    {
+        private int arraysize;
+        private int groups; 
+        private int[] objects;
+        private int[] lastposition;
+        private Color [] objscolor;
+        private int index=-1;
+        bool visual = false;
+        private int selected = -1;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void _btnRandom_Click(object sender, EventArgs e)
+        {
+            arraysize = (int)_nudSize.Value;
+            groups = (int)_nudGroups.Value;
+
+            objects = new int[arraysize];
+
+            Random rnd = new Random();
+
+            for (int i = 0; i < objects.Length; i++)
+                objects[i] = rnd.Next(groups);
+
+            lastposition = new int[groups];
+            objscolor = new Color[groups];
+            index = 0;
+
+            for(int i=0; i<lastposition.Length; i++)
+            {
+                lastposition[i] = -1;
+                objscolor[i] = Color.FromArgb(rnd.Next(256), rnd.Next(256), rnd.Next(256));
+            }
+
+            Draw();
+        }
+
+        private void Draw()
+        {
+            Bitmap bmp = new Bitmap(480, 480);
+
+            using (Graphics g = Graphics.FromImage(bmp))
+            {
+                g.Clear(Color.Black);
+
+                int x = 0;
+                int y = 0;
+
+                for (int i = 0; i < objects.Length; i++)
+                {
+                    g.FillRectangle(new SolidBrush(objscolor[objects[i]]), x + 1, y + 1, 28, 28);
+
+                    if (selected == i)
+                        g.DrawRectangle(Pens.White, x, y, 30, 30);
+
+                    x += 30;
+
+                    if (x > bmp.Width-1)
+                    {
+                        x = 0;
+                        y += 30;
+                    }
+                }
+            }
+
+            _picView.Image = bmp;
+        }
+
+        private void _nudSpeed_ValueChanged(object sender, EventArgs e)
+        {
+            _tmrSolve.Interval = ((10 - (int)_nudSpeed.Value) * 20) + 1;
+        }
+
+        private void _btnVisualSolve_Click(object sender, EventArgs e)
+        {
+            if (index != 0)
+                return;
+
+            _tmrSolve.Enabled = true;
+            _btnRandom.Enabled = false;
+            _btnSolve.Enabled = false;
+            _btnVisualSolve.Enabled = false;
+            _btnStop.Enabled = true;
+            visual = true;
+        }
+
+        private void _btnStop_Click(object sender, EventArgs e)
+        {
+            _tmrSolve.Enabled = false;
+            _btnRandom.Enabled = true;
+            _btnSolve.Enabled = true;
+            _btnVisualSolve.Enabled = true;
+            _btnStop.Enabled = false;
+        }
+
+        private void _btnSolve_Click(object sender, EventArgs e)
+        {
+            visual = false;
+
+            if (index != 0)
+                return;
+
+            while (!Solve()) { };
+
+            Draw();
+        }
+
+        private bool Solve()
+        {
+            if (index >= objects.Length)
+                return true;
+
+            if (lastposition[objects[index]] != -1)
+            {
+                for (int i = 1; i < groups && objects[index - 1] != objects[index]; i++)
+                {
+                    int afterlast = lastposition[objects[index]] + 1;
+
+                    if (afterlast >= index)
+                        continue;
+
+                    int tmp = objects[afterlast];
+
+                    if (visual)
+                        Select(afterlast);
+
+                    objects[afterlast] = objects[index];
+
+                    if (visual)
+                        Select(afterlast);
+
+                    objects[index] = tmp;
+
+                    if (visual)
+                        Select(index);
+
+                    lastposition[objects[afterlast]] = afterlast;
+                }
+            }
+            
+            lastposition[objects[index]] = index;
+
+            index++;
+
+            return false;
+        }
+
+        private void Select(int i)
+        {
+            selected = i;
+            Draw();
+            _picView.Refresh();
+            Thread.Sleep(_tmrSolve.Interval);
+        }
+
+        private void _tmrSolve_Tick(object sender, EventArgs e)
+        {            
+            bool res = Solve();
+            selected = -1;
+            Draw();
+
+            if (res)
+                _btnStop_Click(null, null);
+        }
+
+
+    }
+}
